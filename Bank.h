4f1e22f062e760979225b5dd060f4a1c2ad278f5
Twitch1209 <39025598+Twitch1209@users.noreply.github.com>
2018-10-15 23:18:31 +0800

    Update Bank.h

---
+++ Bank.h
@@ -0,0 +1,35 @@
+#ifndef _Bank_H_
+#define _Bank_H_
+#include"PersonalInfo.h"
+
+
+class Bank
+{
+	bool SavingTime();
+	bool BorrowGT(int LengthOfTime);
+
+
+
+private:
+	PersonalInfo * info;
+};
+
+bool Bank::SavingTime()
+{
+	info.SetVault(info.GetVault() + info.GetRGT());
+	info.SetRGT(0);
+	return true;
+}
+
+bool Bank::BorrowGT(int LengthOfTime)
+{
+	if (LengthOfTime > info.GetVault())
+	{
+		//提示您当前的余额
+		return false;
+	}
+	info.SetRGT(LengthOfTime + info.GetGRT());
+}
+
+
+#endif // !_Bank_H_
